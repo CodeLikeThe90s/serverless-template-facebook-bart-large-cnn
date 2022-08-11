c15df7617fc903d9a8dc05c70dee7c7115ce1bd5
Nelson Chu <csnelsonchu@gmail.com>
2022-08-11 12:02:31 +0800

    setup model pipeline

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = pipeline("summarization", model="facebook/bart-large-cnn", device=device)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -15,12 +15,18 @@ def inference(model_inputs:dict) -> dict:
     global model
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    input = model_inputs.get('input', None)
+    max_length = model_inputs.get('max_length', None)
+    min_length = model_inputs.get('min_length', None)
+    if input == None:
+        return {'message': "No input provided"}
+    if max_length == None:
+        return {'message': "No max_length provided"}
+    if min_length == None:
+        return {'message': "No min_length provided"}
     
     # Run the model
-    result = model(prompt)
+    result = model(input, max_length=max_length, min_length=min_length, do_sample=False)[0]
 
     # Return the results as a dictionary
     return result
